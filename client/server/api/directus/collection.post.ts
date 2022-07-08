74f3604707ea54fd70070bc39fcfece2cb53514a
Craig Harman <craig@craigharman.com>
2022-07-08 15:08:54 +0800

    refactor: handle errors on login and page creation
    Wrap login and page creation in try/catch.

--- client/server/api/directus/collection.post.ts
+++ client/server/api/directus/collection.post.ts
@@ -1,10 +1,15 @@
 import { createPage } from "@nuxtus/generator"
+import { createRouter } from "h3"
 
 export default defineEventHandler(async (event) => {
 	if (process.env.NODE_ENV !== "production") {
 		const body = await useBody(event)
-		// TODO: Wrap in try/catch and return HTTP error if errors
-		createPage(body.collection, body.singleton)
+		try {
+			const collection = body.collection
+			createPage(collection, body.singleton)
+		} catch (err) {
+			throw new Error("Unable to create page: " + err.message)
+		}
 		return {
 			api: "ok",
 		}
