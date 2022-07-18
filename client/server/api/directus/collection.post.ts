f96aea3f4252d3ccde5bb2f4177e5db89c0573ce
Craig Harman <craig@craigharman.com>
2022-07-18 17:09:49 +0800

    build: :arrow_up: update to @nuxtus/generator that allows type creation
    Move type creation to @nuxtus/generator

--- client/server/api/directus/collection.post.ts
+++ client/server/api/directus/collection.post.ts
@@ -1,11 +1,12 @@
-import { createPage } from "@nuxtus/generator"
+import Generator from "@nuxtus/generator"
 
 export default defineEventHandler(async (event) => {
 	if (process.env.NODE_ENV !== "production") {
 		const body = await useBody(event)
+		const nuxtus = new Generator()
 		try {
 			const collection = body.collection
-			createPage(collection, body.singleton)
+			nuxtus.createPage(collection, body.singleton)
 		} catch (err) {
 			console.error(err.message)
 			throw new Error("Unable to create page: " + err.message)
--- client/server/api/directus/field.post.ts
+++ client/server/api/directus/field.post.ts
@@ -1,33 +1,12 @@
-import axios from "axios"
-import fs from "fs"
-import openapiTS from "openapi-typescript"
+import Generator from "@nuxtus/generator"
 
 const directusServer: string =
 	process.env.DIRECTUS_URL || "http://localhost:8055"
 
 export default defineEventHandler(async (event) => {
 	if (process.env.NODE_ENV !== "production") {
-		const config = useRuntimeConfig()
-		// TODO: Can't seem to share login from directusLogin (currently client only plugin)
-		try {
-			const { data } = await axios.post(`${directusServer}/auth/login`, {
-				email: config.public.directusEmail,
-				password: config.public.directusPassword,
-			})
-			axios.defaults.headers.common[
-				"Authorization"
-			] = `Bearer ${data.data.access_token}`
-		} catch (err) {
-			console.error("Failed to login to Directus: " + err.message)
-			throw err
-		}
-		const openapi = await axios.get(`${directusServer}/server/specs/oas`)
-		// console.log(JSON.stringify(openapi.data.components.schemas))
-		const types = await openapiTS(openapi.data)
-		if (!fs.existsSync("interfaces")) {
-			fs.mkdirSync("interfaces")
-		}
-		fs.writeFileSync("interfaces/nuxtus.ts", types)
+		const nuxtus = new Generator()
+		nuxtus.createTypes()
 		return {
 			api: "ok",
 		}
