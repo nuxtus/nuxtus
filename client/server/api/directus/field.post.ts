cf8beda832e11020cd46ec6aa88e55122f7ddaee
Craig Harman <craig@craigharman.com>
2022-07-18 20:27:09 +0800

    refactor: :zap: add missing await to createTypes
    Add await before executing createTypes

--- client/server/api/directus/field.post.ts
+++ client/server/api/directus/field.post.ts
@@ -1,12 +1,9 @@
 import Generator from "@nuxtus/generator"
 
-const directusServer: string =
-	process.env.DIRECTUS_URL || "http://localhost:8055"
-
 export default defineEventHandler(async (event) => {
 	if (process.env.NODE_ENV !== "production") {
 		const nuxtus = new Generator()
-		nuxtus.createTypes()
+		await nuxtus.createTypes()
 		return {
 			api: "ok",
 		}
