e65c732fc43926d5d8d3ca7a6f0523bd53a16c2b
Craig Harman <craig@craigharman.com>
2022-11-10 19:27:50 +0800

    fix: :bug: Update google fonts and remove build step
    Upgraded to google-fonts 2 and remove builkd step

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -3,12 +3,6 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
 	buildModules: [
-		[
-			"@nuxtjs/google-fonts",
-			{
-				/* module options */
-			},
-		],
 		"@nuxtjs/tailwindcss",
 	],
 	modules: ["nuxt-directus", "@nuxtus/nuxt-module"],
